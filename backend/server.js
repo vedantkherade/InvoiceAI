54185c36b652e4f7491c6675612a75bd503167bc
vedantkherade <vedantkherade76@gmail.com>
2026-06-19 13:04:26 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const port = 4000;
 
 // MIDDLEWARES
 app.use(cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     credentials: true
 }));
 app.use(clerkMiddleware());
